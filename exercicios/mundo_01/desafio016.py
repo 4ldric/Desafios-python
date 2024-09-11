b7d225694f7aa5de9f5925781ebbfb3fb012843b
4ldric <academy.wendell@gmail.com>
2024-09-11 11:50:17 -0300

    edição desafios 16 e 17

--- exercicios/mundo_01/desafio016.py
+++ exercicios/mundo_01/desafio016.py
@@ -1,8 +1,19 @@
 #  FAÇA UM PROGRAMA QUE LEIA O COMPRIMENTO DO CATETO OPOSTO E DO CATETO ADJACENTE DE UM TRIANGULO RETANGULO, CALCULE E MOSTRE O COMPRIMENTO DA HIPOTENUSA
 from math import sqrt, pow
 
-cateto_oposto = int(input("informe o comprimento do cateto oposto "))
-cateto_adjacente = int(input("informe o comprimento do cateto adjacente "))
-hipotenusa = sqrt((pow(cateto_oposto,2) + pow(cateto_adjacente, 2)))
+cateto_oposto = float(input("informe o comprimento do cateto oposto "))
+cateto_adjacente = float(input("informe o comprimento do cateto adjacente "))
+hipotenusa = sqrt((pow(cateto_oposto, 2) + pow(cateto_adjacente, 2)))
 
-print(f'a hipotenusa do triangulo e igual a {hipotenusa:.1f}cm')
+print(f'a hipotenusa do triangulo e igual a {hipotenusa:.2f}cm')
+
+
+# RESOLUÇÃO PROFESSOR
+
+from math import hypot
+
+co = float(input("informe o comprimento do cateto oposto "))
+ca = float(input("informe o comprimento do cateto adjacente "))
+hi = hypot(co, ca)
+
+print(f'a hipotenusa do triangulo e igual a {hi:.2f}cm')
--- exercicios/mundo_01/desafio017.py
+++ exercicios/mundo_01/desafio017.py
@@ -1,10 +1,10 @@
 # FAÇA UM PROGRAMA QUE LEIA UM ANGULO QUALQUER E MOSTRE O VALOR DO SENO, COSSENO E TANGENTE DESSE ANGULO.
-import math
+from math import radians, sin, cos, tan
 
 angulo = int(input('informe o angulo: '))
-calc = math.radians(angulo)
-seno = math.sin(calc)
-cosseno = math.cos(calc)
-tangente = math.tan(calc)
+calc = radians(angulo)
+seno = sin(calc)
+cosseno = cos(calc)
+tangente = tan(calc)
 
-print(f'seno, cosseno e tangente do angulo {angulo} respectivamente e {seno}, {cosseno}, {tangente}')
+print(f'seno, cosseno e tangente do angulo {angulo} respectivamente e {seno:.2f}, {cosseno:.2f}, {tangente:.2f}')
