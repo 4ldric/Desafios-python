24cf1f31667a9dde2046c349cf4058057577b71a
4ldric <academy.wendell@gmail.com>
2024-09-11 11:42:17 -0300

    adição desafio 15

--- exercicios/mundo_01/desafio015.py
+++ exercicios/mundo_01/desafio015.py
@@ -1,8 +1,14 @@
 # CRIE UM PROGRAMA QUE LEIA UM NUMERO REAL QUALQUER E MOSTRE NA TELA SUA PORÇAO INTEIRA
 
-import math
+from math import floor
 
 numero_real = float(input("digite um numero: "))
-numero_inteiro = math.floor(numero_real)
+numero_inteiro = floor(numero_real)
 
 print(f'O numero {numero_real} tem a parte inteira {numero_inteiro}')
+
+# SEM IMPORTAÇÃO DE MODULOS
+
+numero = float(input("digite um numero: "))
+
+print(f'O numero {numero} tem a parte inteira {int(numero)}')
