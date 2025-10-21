0ed6b80c7ef88ab9905fa06a300dacef76b13a81
4ldric <academy.wendell@gmail.com>
2025-10-21 11:16:43 -0300

    feat: desafios 55 e 56 resolvidos

--- exercicios/mundo_02/desafio055.py
+++ exercicios/mundo_02/desafio055.py
@@ -1,1 +1,12 @@
 # FAÇA UM PROGRAMA QUE LEIA O PESO DE CINCO PESSOAS. NO FINAL MOSTRE QUAL FOI O MAIOR E O PESO LIDO.
+
+contador = 1
+lista_pesos = []
+
+while contador <= 5:
+    peso = float(input("Informe seu peso: "))
+    lista_pesos.append(peso)
+    contador += 1
+lista_pesos.sort(reverse=True)
+print(f'O maior peso da lista foi: {lista_pesos[0]} Kg\n e o menor peso lido: {lista_pesos[-1]} Kg')
+
--- exercicios/mundo_02/desafio056.py
+++ exercicios/mundo_02/desafio056.py
@@ -3,5 +3,38 @@
 #QUAL E O NOME DO HOMEM MAIS VELHO
 #QUANTAS MULHERES TEM MENOS DE 20 ANOS
 
+idades = []
+pessoas = []
+homem_velho = None
+mulheres = []
+
+for pessoa in range(1,5):
+    nome = input("Informe seu nome: ")
+    idade = int(input("Informe sua idade: "))
+    sexo = input("DIgite seu genero [F/M]: ").lower()
+    idades.append(idade)
+    pessoas.append({
+        "nome": nome,
+        "idade": idade,
+        "sexo": sexo
+    })
+
+media_idades = sum(idades) / len(idades)
+idades.sort(reverse=True)
+maior_idade = idades[0]
+
+for pessoa in pessoas:
+    if pessoa["idade"] == maior_idade and pessoa["sexo"] == "m":
+        homem_velho = pessoa
+    if pessoa["sexo"] == "f" and pessoa["idade"] < 20:
+        mulheres.append(pessoa)
 
 
+if mulheres == []:
+    mensagem = "não há mulheres "
+elif len(mulheres) > 1:
+    mensagem = f"há {len(mulheres)} mulheres"
+else:
+    mensagem = f"há {len(mulheres)} mulher"
+
+print(f'A media de idade do grupo e: {media_idades}\nO homem mais velho do grupo e o {homem_velho['nome']} com {homem_velho['idade']} anos\nE {mensagem} abaixo de 20 anos')
